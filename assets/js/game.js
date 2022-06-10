be5cdb7f7f873a0ea138d5f1ba1e6ea39728fb7e
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-06-10 14:57:32 -0500

    Added localStorage for highscores

--- assets/js/game.js
+++ assets/js/game.js
@@ -132,6 +132,19 @@ var endGame = function() {
     // if the player is still alive, player wins!
     if (playerInfo.health > 0) {
         window.alert("Congratulations! You've survived the game! You now have a score of " + playerInfo.money + ".");
+            // check localStorage for high score, if it's not there, use 0
+        var highScore = localStorage.getItem("highscore");
+        highScore = highScore || 0;
+        // if player has more money than the high score, player has new high score!
+        if (playerInfo.money > highScore) {
+            localStorage.setItem("highscore", playerInfo.money);
+            localStorage.setItem("name", playerInfo.name);
+
+            alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+        }
+        else {
+            alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+        }
     }
     else {
         window.alert("You've lost your robot in battle.");
